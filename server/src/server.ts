8c591f27397db97e0cf3ae627fc6bc2f226cab5c
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2020-09-26 21:21:41 +0200

    heroku

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,6 @@
 import express from "express";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const app = express();
 
